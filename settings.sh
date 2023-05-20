5f0de31b35585b5cb17d1390203a59a962a7a922
PMichnal <110689550+PMichnal@users.noreply.github.com>
2023-05-20 11:05:20 +0200

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,3 +1,4 @@
+# Bunch of colors
 BLUE='\033[0;34m'
 YELLOW='\033[1;33m'
 PURPLE='\033[0;35m'
@@ -7,11 +8,26 @@ CYAN='\033[0;36m'
 WHITE='\033[1;37m'
 NC='\033[0m'
 
+
+JULIA_PKG=no
+
+# Default text editor
+EDITOR="code"
+
+# Default colors
 {
     PYTHON_COLOR=$BLUE
     CPP_COLOR=$YELLOW
     JULIA_COLOR=$PURPLE
 }
 
+#Names of default files 
 CPPDEF="main.cpp"
 PYTHONDEF="main.py"
+
+CONDA_USAGE_CPP=yes
+CONDA_USAGE_PYTHON=yes
+
+CONDA_PACKAGES_CPP=""
+CONDA_PACKAGES_PYTHON="" #Specify packages
+
