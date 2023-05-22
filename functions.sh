52435fde6ba8b6b384eca4c065d8d07a75e5ea9c
PMichnal <110689550+PMichnal@users.noreply.github.com>
2023-05-22 11:22:52 +0200

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -84,27 +84,3 @@ handle_conda() {
     fi
     
 }
-
-    cd ~/Projects/$language_projects/
-    echo "using Pkg; Pkg.generate(\"$project_name\")" | julia
-  else
-    mkdir -p ~/Projects/$language_projects/$project_name
-  fi
-}
-
-
-#there is no Julia template, as its provided by the Pkg.generate() function. Maybe will add it if i need something more specific
-
-
-get_template () {
-    local templ=$1
-    local wd=$(dirname "$0")
-
-    if [ -f "$wd/Templates/$templ" ]
-    then
-        cat "$wd/Templates/$templ"
-    else
-        echo "Error: Template for language not found"
-        exit 1
-    fi
-}
